50db8b7f93e041be952f0c138150c0a0d86e34dc
Jiamin-Zou <jiaminz622@gmail.com>
2023-04-28 12:18:51 -0400

    Completed piece specs

--- reversi/src/piece.js
+++ reversi/src/piece.js
@@ -1,19 +1,23 @@
 /**
  * Initializes the Piece with its color.
  */
+const COLORS = ["white", "black"];
 function Piece (color) {
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    return (this.color === "white") ? "black" : "white";
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    this.color = (this.color === "white") ? "black" : "white";
 };
 
 /**
@@ -21,6 +25,8 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+    // return (this.color === "white") ? "W" : "B";
+    return this.color[0].toUpperCase();
 };
 
 // DON'T TOUCH THIS CODE
